936e3df85d1f8a87d03b00e32fb82151e96e35a5
Anonymous <Anonymous>
2021-04-30 00:06:29 +0800

    Update data

--- controllers/RESTApi.cc
+++ controllers/RESTApi.cc
@@ -7,11 +7,20 @@ void RESTApi::get(const HttpRequestPtr &req,
 {
     LOG_DEBUG << "Action (Get):" << action;
     Json::Value ret;
-    ret["result"]="ok";
-    ret["message"]="Hello World";
-    std::string command{"echo HELO"};
-    
-    ret["exitcode"]=std::system(command.c_str());
+    ret["result"] = "ok";							//Message 1
+    ret["message"] = "Hello World";		//Message 2
+
+		std::string command{"echo HELO"};	//The command you would like to run
+
+		if ( "Autoware.command.code1" == action ){
+				command = "echo Command A";
+		} else if ( "Autoware.command.code2" == action ){
+				command = "echo Command B";
+		} else {
+				command = "echo Command C";
+		}
+
+    ret["exitcode"] = std::system(command.c_str());	//Store the exit code
 
     auto resp = HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
@@ -26,7 +35,7 @@ void RESTApi::post(const HttpRequestPtr &req,
     ret["result"]="ok";
     ret["message"]="Hello World";
     std::string command{"echo HELO"};
-  
+
     int rs = std::system(command.c_str());
     ret["exitcode"]=rs;
     auto resp = HttpResponse::newHttpJsonResponse(ret);
